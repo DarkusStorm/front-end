650f502adaf89cb27529972236b9458bdbbcb09d
DarkusStorm <tico.alvessilva2008@gmail.com>
2026-04-28 16:42:40 -0300

    refact: correção do console.log

--- tarefa-biblioteca/script.js
+++ tarefa-biblioteca/script.js
@@ -1,10 +1,10 @@
-function biblioteca(){
+function biblioteca() {
     const percy = {
         titulo: "Percy Jackson e o Ladrão de Raios",
         autor: "Rick Riordan",
         disponivel: true,
         imagem: "https://ocondadostore.com/wp-content/uploads/2020/06/Ladr%C3%A3o-de-Raios.jpg"
-    };
+    }
 
     const harry = {
         titulo: "Harry Potter e a Pedra Filosofal",
@@ -28,14 +28,14 @@ function biblioteca(){
     }
 
     const livros = [percy, harry, aneis, narnia];
-    const resultados = document.querySelector("#resultadosBusca");
+    const resultados = document.getElementById("resultadosBusca");
 
-    function exibirLivros(livros){
+    function exibirLivros(livros) {
 
         livros.forEach(livro => {
             const card = document.createElement("div");
-            card.classList.add("card")
-            
+            card.classList.add("card");
+
             const titulo = document.createElement("h3");
             titulo.textContent = livro.titulo;
 
@@ -44,32 +44,31 @@ function biblioteca(){
             imagem.height = 200;
 
             const autor = document.createElement("p");
-            autor.textContent = `Autor: ${livro.autor}`;
+            autor.textContent = `Autor(a): ${livro.autor}`;
 
             const status = document.createElement("p");
-            if (livro.disponivel == true){
+            if (livro.disponivel == true) {
                 status.textContent = "Status: Disponível";
-                status.classList.add("livro-disponivel")
+                status.classList.add("livro-disponivel");
             }
             else {
                 status.textContent = "Status: Emprestado";
-                status.classList.add("livro-emprestado")
+                status.classList.add("livro-emprestado");
             }
 
             const botaoReservar = document.createElement("button");
             botaoReservar.textContent = "Reservar Livro";
 
-            if (livro.disponivel == true){
+            if (livro.disponivel == true) {
                 botaoReservar.addEventListener("click", () => {
                     botaoReservar.textContent = "Reservado!";
                     status.textContent = "Status: Emprestado";
                     botaoReservar.disabled = true;
                     livro.disponivel = false;
-                    status.classList.add("livro-emprestado")
-                    console.log(`Livro de cadastro: "${livro.titulo}" reservado com sucesso!`)
-    });
-}
-
+                    status.classList.add("livro-emprestado");
+                    console.log(`Livro de cadastro: ${livro.titulo} reservado com sucesso!`);
+                });
+            }
             card.appendChild(imagem);
             card.appendChild(titulo);
             card.appendChild(autor);
@@ -78,6 +77,5 @@ function biblioteca(){
             resultados.appendChild(card);
         });
     }
-
     exibirLivros(livros);
 }
